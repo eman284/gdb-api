703d42c050242f7caf95c85c9a631787d68bcfd9
Iman Hussien <a.ihussien@dev.thiqah.sa>
2022-02-17 06:45:26 +0200

    modify file

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
-  const config = new DocumentBuilder()
-    .setTitle('Post')
-    .setDescription('The task API description')
-    .setExternalDoc('/api-json', '/api-json')
-    .addServer(`http://localhost:${port}`)
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  const isDevelopment = configService.get('STAGE') === 'dev';
+
+  app.enableCors();
+
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('Task Management')
+    .setDescription('The Task Management API')
     .setVersion('1.0')
-    .addTag('Post Api')
-    .build();
+    .setExternalDoc('api-json', 'api-json')
+    .addBearerAuth();
+  documentBuilder.addServer('https://training-gdb-api.herokuapp.com');
+  if (isDevelopment) {
+    documentBuilder.addServer(`http://localhost:${port}`);
+  }
+  const config = documentBuilder.build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  app.enableCors();
   await app.listen(port);
 }
 bootstrap();
