995f340eab40837d20832e3a59bb2d49a18648cc
Iman Hussien <a.ihussien@dev.thiqah.sa>
2022-02-17 21:24:16 +0200

    add new changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,38 +2,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { configValidationSchema } from 'config.schema';
 import { PostModule } from './post/post.module';
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      envFilePath: [`environments/.env.${process.env.STAGE}`],
-      validationSchema: configValidationSchema,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-
-      useFactory: (configService: ConfigService) => {
-        const isProduction = configService.get('STAGE') === 'prod';
-        return {
-          // secure connection
-          ssl: isProduction,
-          extra: {
-            ssl: isProduction ? { rejectUnauthorized: false } : null,
-          },
-          type: 'postgres',
-          entities: ['**/*.entity{ .ts,.js}'],
-          autoLoadEntities: true,
-          synchronize: true,
-          host: configService.get('DB_HOST'),
-          port: configService.get('DB_PORT'),
-          username: configService.get('DB_USERNAME'),
-          password: configService.get('DB_PASSWORD'),
-          database: configService.get('DB_DATABASE'),
-        };
-      },
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      entities: ['**/*.entity{ .ts,.js}'],
+      autoLoadEntities: true,
+      synchronize: true,
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postDB',
     }),
     PostModule,
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
-  const isDevelopment = configService.get('STAGE') === 'dev';
+  const port = 3000;
 
   app.enableCors();
 
   const documentBuilder = new DocumentBuilder()
-    .setTitle('Task Management')
-    .setDescription('The Task Management API')
+    .setTitle('Post Api')
+    .setDescription('The Post API')
     .setVersion('1.0')
     .setExternalDoc('api-json', 'api-json')
     .addBearerAuth();
-  documentBuilder.addServer('https://training-gdb-api.herokuapp.com');
-  if (isDevelopment) {
-    documentBuilder.addServer(`http://localhost:${port}`);
-  }
+  documentBuilder.addServer(`http://localhost:${port}`);
   const config = documentBuilder.build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
